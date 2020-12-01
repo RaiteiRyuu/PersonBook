26579ea1e7d99270fc2e1c972cb6c2dc9ab7d4c5
raiteiryuu <kitt.cheung01@gmail.com>
2020-12-01 11:00:32 +0100

    Add project files.

---
+++ PersonBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
